b3e3dbd9486c03ed55b7d3d34485e82dc446e2ab
hassank <hk22793@gmail.com>
2018-05-29 09:07:26 -0700

    Adjust SnowPain scene.

---
+++ Assets/PenguinSpawner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PenguinSpawner : MonoBehaviour {
+	public GameObject penguin;
+	public float xRange;
+	public float zRange;
+
+	private static PenguinSpawner Instance;
+	private Vector3 spawnLocation;
+
+	void Awake() {
+		if (Instance == null)
+			Instance = this;
+
+		spawnLocation = gameObject.transform.position;
+	}
+
+	// Use this for initialization
+	void Start () {
+		int initialPenguins = Random.Range (4, 11);
+		for (int i = 0; i < initialPenguins; i++)
+			Spawn ();
+
+		StartCoroutine (SpawnCycle ());
+	}
+
+	private IEnumerator SpawnCycle() {
+		yield return new WaitForSeconds (1f);
+
+		while (true) {
+			Spawn ();
+			yield return new WaitForSeconds (3f);
+		}
+	}
+
+	private void Spawn() {
+		Vector3 spawnPosition = GetSpawnPosition ();
+		Instantiate (penguin, spawnPosition, transform.rotation);
+	}
+
+	private Vector3 GetSpawnPosition() {
+		spawnLocation.x = gameObject.transform.position.x + Random.Range (-xRange, xRange);
+		spawnLocation.z = gameObject.transform.position.z + Random.Range (-zRange, zRange);
+
+		return spawnLocation;
+	}
+}
